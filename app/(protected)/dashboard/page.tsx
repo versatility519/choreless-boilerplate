3d0fc9eeb7c67b62d7667ea4344834bb12878094
stefan <veteransmut519@gmail.com>
2024-10-02 04:16:23 +1000

    initial choreless project

---
+++ app/(protected)/dashboard/page.tsx
@@ -0,0 +1,31 @@
+import { getCurrentUser } from "@/lib/session";
+import { constructMetadata } from "@/lib/utils";
+import { Button } from "@/components/ui/button";
+import { DashboardHeader } from "@/components/dashboard/header";
+import { EmptyPlaceholder } from "@/components/shared/empty-placeholder";
+
+export const metadata = constructMetadata({
+  title: "Dashboard – SaaS Starter",
+  description: "Create and manage content.",
+});
+
+export default async function DashboardPage() {
+  const user = await getCurrentUser();
+
+  return (
+    <>
+      <DashboardHeader
+        heading="Dashboard"
+        text={`Current Role : ${user?.role} — Change your role in settings.`}
+      />
+      <EmptyPlaceholder>
+        <EmptyPlaceholder.Icon name="post" />
+        <EmptyPlaceholder.Title>No content created</EmptyPlaceholder.Title>
+        <EmptyPlaceholder.Description>
+          You don&apos;t have any content yet. Start creating content.
+        </EmptyPlaceholder.Description>
+        <Button>Add Content</Button>
+      </EmptyPlaceholder>
+    </>
+  );
+}
---
+++ config/site.ts
@@ -0,0 +1,47 @@
+import { SidebarNavItem, SiteConfig } from "types";
+import { env } from "@/env.mjs";
+
+const site_url = env.NEXT_PUBLIC_APP_URL;
+
+export const siteConfig: SiteConfig = {
+  name: "Choreless",
+  description:
+    "Choreless is a platform that helps you manage your chores and get paid for them.",
+  url: site_url,
+  ogImage: `${site_url}/_static/og.jpg`,
+  links: {
+    twitter: "https://twitter.com/miickasmt",
+    github: "https://github.com/mickasmt/next-saas-stripe-starter",
+  },
+  mailSupport: "support@saas-starter.com",
+};
+
+export const footerLinks: SidebarNavItem[] = [
+  {
+    title: "Company",
+    items: [
+      { title: "About", href: "#" },
+      { title: "Enterprise", href: "#" },
+      { title: "Terms", href: "/terms" },
+      { title: "Privacy", href: "/privacy" },
+    ],
+  },
+  {
+    title: "Product",
+    items: [
+      { title: "Security", href: "#" },
+      { title: "Customization", href: "#" },
+      { title: "Customers", href: "#" },
+      { title: "Changelog", href: "#" },
+    ],
+  },
+  {
+    title: "Docs",
+    items: [
+      { title: "Introduction", href: "#" },
+      { title: "Installation", href: "#" },
+      { title: "Components", href: "#" },
+      { title: "Code Blocks", href: "#" },
+    ],
+  },
+];
